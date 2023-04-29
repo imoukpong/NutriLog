bbeaca1f667325b1894ffa74dd3282d63658e885
ReasonablePhantom <57421138+ReasonablePhantom@users.noreply.github.com>
2023-04-28 22:42:42 -0700

    add a class to support when click the main_page's break_button to jump to the breakfast.xml

--- app/src/main/java/com/example/nutrilog/MainActivity.kt
+++ app/src/main/java/com/example/nutrilog/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.nutrilog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,22 @@ class MainPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mainpage)
+    }
+}
+
+class BreakButton : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainpage)
+
+        // 获取按钮对象
+        val button = findViewById<Button>(R.id.BreakButton)
+
+        // 设置按钮的点击事件
+        button.setOnClickListener {
+            // 创建一个Intent对象，跳转到第二个Activity
+            val intent = Intent(this, BreakButton::class.java)
+            startActivity(intent)
+        }
     }
 }
